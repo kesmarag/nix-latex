8fdbf5d5dfcf736b7daeb07efed5b7590de39161
Costas Smaragdakis <kesmarag@gmail.com>
2025-08-25 19:05:15 +0300

    update

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Kesmarag's LaTeX environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      
+      myTexLive = pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-small  # A good minimal starting point
+          latexmk       # For automated builds (running lualatex, biber, etc.)
+          biber         # A modern bibliography tool
+          biblatex      # A biblatex-compatible bibliography system
+
+          # Add any other packages your document needs here:
+          amsmath
+          amsfonts
+          enumitem
+          algorithms
+          minitoc
+          wrapfig
+          capt-of
+          greek-fontenc
+          algorithmicx
+          amsaddr
+          accents
+          subfigure
+          stmaryrd
+          systeme
+          cancel
+          cleveref
+          lipsum
+          extarrows
+          relsize
+          bbm
+          blkarray
+          floatrow
+          doublestroke
+          xstring
+          simplekv
+          subfiles
+          babel-greek
+          gfsartemisia
+          tex-gyre
+          cm-super
+          cbfonts
+          collection-langgreek
+          ;
+      };
+
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # The packages available in the shell
+        buildInputs = [
+          myTexLive
+        ];
+
+        # A hook that runs when you enter the shell.
+        shellHook = ''
+          echo "Welcome to your reproducible LaTeX environment!"
+          echo "Run 'latexmk -xelatex main.tex' to build your document."
+        '';
+      };
+    };
+}
